659fe4b506fff5f2784b9a997d3a9fd443a01207
f3ltron <florent.giraud.16@gmail.com>
2019-01-10 20:59:32 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+// constant config for app
 import { NAME } from './constants'
+// types config
 import { IDocgenOptions, IVuePressOpenContext } from './types'
+// default options for each env
 import { setDefaultOptions } from './utils/environment'
+// import logger from consola init
 import logger from './utils/logger'
+
+// all builds config
 import {
   buildDirContext,
   buildComponentContext,
@@ -10,10 +16,30 @@ import {
   buildWebpackConfig,
   buildEnhanceApp,
 } from './build'
+
 import { removeDir } from './utils/file'
 
+/**
+ * the module.export needed by vuepress
+ * @description receive options
+ * @description receive ctx or context
+ * watch the types for more infos of what you receive
+ *
+ * @params options
+ * @params ctx
+ *
+ *  */
 module.exports = (options: IDocgenOptions, ctx: IVuePressOpenContext) => {
+  /**
+   *  initOptions will update the rootdir options by options pass
+   *  or by the current dir
+   * */
+
   setDefaultOptions(options)
+
+  /**
+   * if definitely no rootDir just crash
+   */
   if (typeof options.rootDir !== 'string') {
     logger.error(
       new Error(
@@ -23,6 +49,9 @@ module.exports = (options: IDocgenOptions, ctx: IVuePressOpenContext) => {
     process.exit(1)
   }
 
+  /**
+   * reference directly for more readability and controlled bug if there is
+   */
   const dirContext = buildDirContext({
     rootDir: options.rootDir as string,
     include: options.include,
@@ -30,6 +59,8 @@ module.exports = (options: IDocgenOptions, ctx: IVuePressOpenContext) => {
     prefix: options.prefix,
     ctx,
   })
+
+  logger.log('=======================================')
 
   const componentContextMap = buildComponentContext({
     dirContext,
