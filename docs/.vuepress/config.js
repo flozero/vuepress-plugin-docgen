fed3cb1bd7da7cd3e2ee37e48a92e89e2153b37e
SangND <dacsang97@gmail.com>
2019-01-14 23:41:20 -0800

    chore - update title docs page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    title: 'vuepress-plugin-component-docgen',
-    description: 'Preview your components',
-    themeConfig: {
-        lastUpdated: 'Last Updated',
-        search: false,
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Github', link: 'https://github.com/youngtailors/vuepress-plugin-component-docgen' },
-        ],
-    },
-    
+  title: 'VuePress Docgen',
+  description: 'Preview your components',
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    search: false,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Github', link: 'https://github.com/youngtailors/vuepress-plugin-component-docgen' },
+    ],
+  },
 }
