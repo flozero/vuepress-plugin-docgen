2e52060584589e3e9403fc80db4a567b98186e43
elevatebart <ledouxb@me.com>
2019-05-11 11:40:07 -0500

    style: add prettier config

--- src/builders/additionnalPages/buildComponentsPages.js
+++ src/builders/additionnalPages/buildComponentsPages.js
@@ -1,83 +1,85 @@
-const { parse } = require('vue-docgen-api');
-const fs = require('fs');
+const { parse } = require('vue-docgen-api')
+const fs = require('fs')
 
-const { getCompiledTemplateWithHbs } = require('../handlebars');
-const VueParser = require('../../parser/vue');
+const { getCompiledTemplateWithHbs } = require('../handlebars')
+const VueParser = require('../../parser/vue')
 
-const {getFileNameFromAbsolutePath} = require("../../extractors/pathReader")
-const {dropVueExtension} = require("../../extractors/name")
+const { getFileNameFromAbsolutePath } = require('../../extractors/pathReader')
+const { dropVueExtension } = require('../../extractors/name')
+
+const wrapExampleInVueLive = require('../../utils/wrapExampleInVueLive')
 
 const buildIndexPageComponent = finalContext => {
-  const contentTemplate = finalContext.options.globalIndexComponentTemplate
-  return {
-    path: `/${finalContext.options.sideBarName}/`,
-    content: getCompiledTemplateWithHbs(finalContext, contentTemplate)
-  }
-};
+    const contentTemplate = finalContext.options.globalIndexComponentTemplate
+    return {
+        path: `/${finalContext.options.sideBarName}/`,
+        content: getCompiledTemplateWithHbs(finalContext, contentTemplate),
+    }
+}
 
 const buildComponentPage = (absolutePath, parentPath, finalContext) => {
-  let preview = '';
+    let preview = ''
 
-  const componentInfo = parse(absolutePath);
-  const componentPreviewCompiled = getCompiledTemplateWithHbs(
-        componentInfo, 
-        finalContext.options.componentsDocsTemplate);
+    const componentInfo = parse(absolutePath)
+    const componentPreviewCompiled = getCompiledTemplateWithHbs(
+        componentInfo,
+        finalContext.options.componentsDocsTemplate,
+    )
 
-  const readedComponent = fs.readFileSync(absolutePath, { encoding: 'utf-8'});
+    const readedComponent = fs.readFileSync(absolutePath, { encoding: 'utf-8' })
 
-  let componentName = getFileNameFromAbsolutePath(absolutePath)
-  componentName = dropVueExtension(componentName);
+    let componentName = getFileNameFromAbsolutePath(absolutePath)
+    componentName = dropVueExtension(componentName)
 
-  const vueParser = new VueParser({
-    source: readedComponent,
-    fileName: componentName,
-  });
+    const vueParser = new VueParser({
+        source: readedComponent,
+        fileName: componentName,
+    })
 
-  const docsBlock = vueParser.getCustomBlock('docs');
+    const docsBlock = vueParser.getCustomBlock('docs')
 
-  if (docsBlock) preview = finalContext.options.docsBlockTemplate(docsBlock);
-
-  let content = componentPreviewCompiled;
-  content += preview;
-
-  return {
-    path: `${parentPath + componentName}.html`,
-    content,
-  };
-};
-
-module.exports = (finalContext) => {
-  const ctx = finalContext.componentsPathContext;
-  const { componentsDir } = finalContext.options;
-  const componentsPages = [];
-  const prefix = finalContext.options.sideBarName
-
-  Object.keys(ctx).forEach((k) => {
-    if (k === 'children') {
-      ctx[k].forEach((componentRelativePath) => {
-        componentsPages.push(
-          buildComponentPage(
-            `${componentsDir}/${componentRelativePath}`,
-            `/${prefix}/`,
-            finalContext
-          ),
-        );
-      });
-    } else {
-      ctx[k].children.forEach((componentRelativePath) => {
-        componentsPages.push(
-          buildComponentPage(
-            `${componentsDir}/${k}/${componentRelativePath}`,
-            `/${prefix}/${k}/`,
-            finalContext
-          ),
-        );
-      });
+    if (docsBlock && docsBlock.content) {
+        const docWithWrappedExamples = wrapExampleInVueLive(docsBlock.content)
+        preview = finalContext.options.docsBlockTemplate(docWithWrappedExamples)
     }
-  });
 
-  return [
-    buildIndexPageComponent(finalContext),
-    ...componentsPages,
-  ];
-};
+    let content = componentPreviewCompiled + preview
+
+    return {
+        path: `${parentPath + componentName}.html`,
+        content,
+    }
+}
+
+module.exports = finalContext => {
+    const ctx = finalContext.componentsPathContext
+    const { componentsDir } = finalContext.options
+    const componentsPages = []
+    const prefix = finalContext.options.sideBarName
+
+    Object.keys(ctx).forEach(k => {
+        if (k === 'children') {
+            ctx[k].forEach(componentRelativePath => {
+                componentsPages.push(
+                    buildComponentPage(
+                        `${componentsDir}/${componentRelativePath}`,
+                        `/${prefix}/`,
+                        finalContext,
+                    ),
+                )
+            })
+        } else {
+            ctx[k].children.forEach(componentRelativePath => {
+                componentsPages.push(
+                    buildComponentPage(
+                        `${componentsDir}/${k}/${componentRelativePath}`,
+                        `/${prefix}/${k}/`,
+                        finalContext,
+                    ),
+                )
+            })
+        }
+    })
+
+    return [buildIndexPageComponent(finalContext), ...componentsPages]
+}
--- test/options.test.js
+++ test/options.test.js
@@ -1,92 +1,91 @@
 /* eslint-disable */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
 const {
     isGoodTyped,
     isOptionsAlltrue,
     regroupOptions,
-    isAllKeyGoodTyped
-} = require("../src/extractors/options");
-const logger = require("../src/utils/logger")
+    isAllKeyGoodTyped,
+} = require('../src/extractors/options')
+const logger = require('../src/utils/logger')
 
 // disable error log provide by parsers for readability of tests. Comment if you want full logs
 logger.pause()
 
-describe("isGoodTyped()", function() {
+describe('isGoodTyped()', function() {
     it('should return false when key is not good typed', function() {
-        expect(isGoodTyped('componentsDir', {'componentsDir': {}})).to.be.false
+        expect(isGoodTyped('componentsDir', { componentsDir: {} })).to.be.false
     })
     it('should return true when key is well typed', function() {
-        expect(isGoodTyped('componentsDir', {'componentsDir': 'asdasdklaj'})).to.be.true
+        expect(isGoodTyped('componentsDir', { componentsDir: 'asdasdklaj' })).to
+            .be.true
     })
 })
 
-describe("isOptionsAlltrue()", function() {
-    
-    it("should return false because all found with required are not true", function(){
+describe('isOptionsAlltrue()', function() {
+    it('should return false because all found with required are not true', function() {
         const opt = {
-            test1: {found: true},
-            test2: {found: false},
+            test1: { found: true },
+            test2: { found: false },
             test3: {
                 required: true,
-                found: false
+                found: false,
             },
         }
         expect(isOptionsAlltrue(opt)).to.be.false
     })
 
-    it("should return true because all found true", function(){
+    it('should return true because all found true', function() {
         const opt = {
-            test1: {found: true},
-            test2: {found: true},
+            test1: { found: true },
+            test2: { found: true },
         }
         expect(isOptionsAlltrue(opt)).to.be.true
     })
 })
 
 describe('regroupOptions()', function() {
-
     const resultNeeded = {
         test: 'asdasds',
-        p: 'asdkjd'
+        p: 'asdkjd',
     }
 
     const givenOpts = {
-        test: 'asdasds'
+        test: 'asdasds',
     }
 
-    it("should regroup to obj with the default values", function() {
+    it('should regroup to obj with the default values', function() {
         const options = {
             p: {
-                default: 'asdkjd'
-            }
+                default: 'asdkjd',
+            },
         }
         expect(regroupOptions(givenOpts, options)).to.deep.equal(resultNeeded)
     })
 
-    it("should regroup to obj with the default values", function() {
+    it('should regroup to obj with the default values', function() {
         const options = {
-            p: 'asdkjd'
+            p: 'asdkjd',
         }
 
-        expect(regroupOptions(givenOpts, options)).to.not.deep.equal(resultNeeded)
+        expect(regroupOptions(givenOpts, options)).to.not.deep.equal(
+            resultNeeded,
+        )
     })
 })
 
-
 describe('isAllKeyGoodTyped()', function() {
-
     it('should return false when one error about type', function() {
         const obj1 = {
-            componentsDir: "asdsddad",
-            debug: "asdsds",
-            sideBarName: "asda"
+            componentsDir: 'asdsddad',
+            debug: 'asdsds',
+            sideBarName: 'asda',
         }
 
         const obj2 = {
-            componentsDir: "asdsddad",
+            componentsDir: 'asdsddad',
             debug: true,
-            sideBarName: false
+            sideBarName: false,
         }
         expect(isAllKeyGoodTyped(obj1)).to.be.false
         expect(isAllKeyGoodTyped(obj2)).to.be.false
@@ -94,12 +93,11 @@ describe('isAllKeyGoodTyped()', function() {
 
     it('should return true', function() {
         const obj = {
-            componentsDir: "asdsddad",
+            componentsDir: 'asdsddad',
             debug: true,
-            sideBarName: "asda"
+            sideBarName: 'asda',
         }
 
         expect(isAllKeyGoodTyped(obj)).to.be.true
     })
 })
-
