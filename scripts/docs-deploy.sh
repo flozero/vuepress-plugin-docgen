12e898ff2a8894ec404b937ee63cb19423570e6d
f3ltron <florent.giraud.16@gmail.com>
2019-06-08 18:07:06 +0200

    fix: 🐛 package.json modification

---
+++ scripts/docs-deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+cd packages/docs
+
+# build
+yarn run docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/f3ltron/vuepress-plugin-docgen.git master:gh-pages
+
+cd -
