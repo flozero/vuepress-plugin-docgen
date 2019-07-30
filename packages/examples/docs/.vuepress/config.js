1b4f2c21877755599a22f9404effa42ed806fe14
f3ltron <florent.giraud.16@gmail.com>
2019-07-30 09:05:58 +0200

    fix(config): fix vuepress example config

--- packages/examples/docs/.vuepress/config.js
+++ packages/examples/docs/.vuepress/config.js
@@ -11,22 +11,20 @@ module.exports = {
       }
     ],
     [
-      [
-        // you can use here 'docgen' instead of the require
-        require("vuepress-plugin-docgen"),
-        {
-          componentsDir: path.join(__dirname, "../../src/components"),
-          // debug: true,
-        }
-      ]
-    ],
-    themeConfig: {
+      // you can use here 'docgen' instead of the require
+      require("vuepress-plugin-docgen"),
+      {
+        componentsDir: path.join(__dirname, "../../src/components"),
+        // debug: true,
+      }
+    ]
+  ],
+  themeConfig: {
       nav: [
         { text: 'Home', link: '/' },
         { text: "Components Examples", link: "/components/" },
         { text: 'Github', link: 'https://github.com/f3ltron/vuepress-plugin-docgen' }
-      ],
-      sidebar: {},
+      ]
     },
     base: "/vuepress-plugin-docgen/"
 }
